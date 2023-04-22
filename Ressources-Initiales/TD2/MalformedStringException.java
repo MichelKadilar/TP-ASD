ddbf3867348920e4168958206a744e93aede0a33
Michel KADILAR <michel.kadilar1@gmail.com>
2023-04-22 18:42:05 +0200

    Initial commit

---
+++ Ressources-Initiales/TD2/MalformedStringException.java
@@ -0,0 +1,7 @@
+package td2.TD2;
+
+public class MalformedStringException extends Exception {
+    public MalformedStringException(String child) {
+        super(child);
+    }
+}
---
+++ src/td2/TD2/TestClass.java
@@ -0,0 +1,2 @@
+package td2.TD2;public class TestClass {
+}
